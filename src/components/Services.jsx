ea271f68a5f9fa9e7114a6cdc38a13a25d7f737b
Kethankrk <kethankrk@gmail.com>
2023-11-08 21:22:21 +0530

    updated image path

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -4,7 +4,7 @@ export default function Services({ name, description, imageLink }) {
   return (
     <div className="card card-compact md:w-[300px] lg:w-[400px] bg-base-200 shadow-xl hover:bg-primary-focus hover:text-base-100">
       <div className="pt-5 pl-10">
-        <img src={`/${imageLink}`} alt="Shoes" className="h-[80px]" />
+        <img src={`${imageLink}`} alt="Shoes" className="h-[80px]" />
       </div>
       <div className="card-body">
         <h2 className="card-title">{name}</h2>
